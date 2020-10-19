1c89f3c764d5b54cbcf85a4fc12de25968dd0c47
Kirill Leontev <39034228+Rybasher@users.noreply.github.com>
2020-10-19 18:25:08 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+;
+function ibg() {
+
+	$.each($('.ibg'), function (index, val) {
+		if ($(this).find('img').length > 0) {
+			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+		}
+	});
+}
+
+ibg();;
+$(".icon__menu").click(function () {
+	$(this).toggleClass("active");
+	$('.menu__body').toggleClass('active');
+	$('body').toggleClass('lock');
+});
+
+$(document).ready(function () {
+	$('.slider').slick({
+		arrows: false,
+		dots: true
+	});
+});;
+function testWebP(callback) {
+
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+	if (support == true) {
+		document.querySelector('body').classList.add('webp');
+	} else {
+		document.querySelector('body').classList.add('no-webp');
+	}
+});
